960736c0041556e4ad572f712e1f96b7fb78308e
Eyþór Magnússon <eysi09@gmail.com>
2016-08-10 11:20:16 +0000

    Update to react-native 0.26.

--- Camera.js
+++ Camera.js
@@ -1,8 +1,8 @@
-import React, {
-  Component,
+import React, { Component, PropTypes } from 'react';
+
+import {
   NativeAppEventEmitter,
   NativeModules,
-  PropTypes,
   StyleSheet,
   requireNativeComponent,
   View,
